16bedc519d6e3a2c806c7965889d56c7afdddd3f
Josiane-cmd <halimedou2003@gmail.com>
2026-07-16 20:20:41 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $db   = 'mglsi_news';
 $user = 'mglsi_user';
-$pass = 'passer'; // Le mot de passe défini dans ton script SQL
+$pass = 'passer'; 
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
@@ -93,22 +93,5 @@ $articles = $stmtArt->fetchAll();
         <p>Copyright &copy; DGI 2019</p>
     </footer>
 
-</body>
-</html>
-
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
 </body>
 </html>
